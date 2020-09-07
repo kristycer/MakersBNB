0240c6b32599bb70301f1b534ae37b5799053abd
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-07 15:00:01 +0100

    Build: project structure

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require 'sinatra/base'
 class MakersBNB < Sinatra::Base
 
     get '/' do
-        'Hello World!'
+      'Hello World!'
     end 
-
+  
     run! if app_file == $0
-
-end 
+  
+  end 
