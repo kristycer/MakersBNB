ee60aedf476115e951e6f0eeca2f54d600234cac
kristyna cervenkova <kristyna275@gmail.com>
2020-09-07 17:07:53 +0200

    added sign up page

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require 'sinatra/base'
 class MakersBNB < Sinatra::Base
 
     get '/' do
-      'Hello World!'
+    
+      erb :index
+    end 
+
+    post '/sign_up' do
+        redirect '/spaces'
+    end 
+
+    get '/spaces' do
+        erb :spaces
     end 
   
     run! if app_file == $0
