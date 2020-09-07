0905807492d6ee37877b3118f2051d94b3c80667
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-07 17:01:02 +0100

    Build: built user login and create methods

---
+++ models/user.rb
@@ -0,0 +1,36 @@
+require './models/database_connection'
+require 'bcrypt'
+
+class User 
+
+  attr_reader :id, :name, :email
+
+  def initialize(id:, name:, email:)
+    @id = id
+    @name = name
+    @email = email
+  end
+
+  def self.create(name:, email:, password:)
+    encrypted_password = BCrypt::Password.create(password)
+    user = DatabaseConnection.query(
+      "Insert INTO users (name, email, password) 
+      VALUES ('#{name}', '#{email}', '#{encrypted_password}') RETURNING id, name, email;")
+    User.new(id: user[0]['id'], name: user[0]['name'], email: user[0]['email'])
+  end
+
+  def self.find(id:)
+    search = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
+    User.new(id: search[0]['id'], name: search[0]['name'], email: search[0]['email'])
+  end
+
+  def self.authenticate(email:, password:)
+    search = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
+
+    return unless search.any?
+
+    return unless BCrypt::Password.new(search[0]['password']) == password
+
+    User.new(id: search[0]['id'], email: search[0]['email'], name: search[0]['name'])
+  end  
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,37 @@
+require './models/user'
+
+describe User do
+  describe '.create' do
+    it 'creates a new user' do
+      user = User.create(name: 'Example', email: 'example@me.com', password: 'password123')
+       
+      expect(user.name).to eq 'Example'
+      expect(user.email).to eq 'example@me.com'
+    end
+  end
+
+  describe '.find' do
+    it 'finds the user by an id' do
+      User.create(name: 'Example', email: 'example@me.com', password: 'password123')
+      user = User.find(id: 1)
+
+      expect(user.name).to eq 'Example'
+      expect(user.id).to eq '1'
+    end
+  end
+
+  describe '.authenticate' do
+    it 'returns a user given correct sign in details' do
+      user = User.create(name: 'Example', email: 'example@me.com', password: 'password123')
+      authenticated_user = User.authenticate(email: 'example@me.com', password: 'password123')
+
+      expect(authenticated_user.id).to eq user.id
+    end
+
+    it 'does not returns a user given correct sign in details' do
+      User.create(name: 'Example', email: 'example@me.com', password: 'password123')
+
+      expect(User.authenticate(email: 'wrong@me.com', password: 'password123')).to be_nil
+    end
+  end
+end
