94a1839d351734dcf966a5d21df327dfffaa2629
Iliyan-Y <maximizebg@gmail.com>
2020-09-08 14:04:59 +0100

    Listing all the spaces

--- models/space.rb
+++ models/space.rb
@@ -2,9 +2,10 @@ require './models/database_connection'
 
 class Space
 
-  attr_reader :name, :description, :location, :price, :available_from, :available_to, :ower
+  attr_reader :id, :name, :description, :location, :price, :available_from, :available_to, :owner
 
-  def initialize(name:, description:, location:, price:, available_from:, available_to:, owner:)
+  def initialize(id:, name:, description:, location:, price:, available_from:, available_to:, owner:)
+    @id = id
     @name = name
     @description = description
     @location = location
@@ -15,8 +16,19 @@ class Space
   end
 
   def self.create(name:, description:, location:, price:, available_from:, available_to:, owner:)
-    space = DatabaseConnection.query("INPUT INTO spaces(name, description, location, price, available_from, available_to, ower) VALUES('#{}','#{}','#{}','#{}','#{}','#{}','#{}') RETURNING id, name, description, location, price, available_from, available_to, owner;")
+    space = DatabaseConnection.query("INSERT INTO spaces(name, description, location, price, available_from, available_to, owner) VALUES('#{name}','#{description}','#{location}','#{price}','#{available_from}','#{available_to}','#{owner}') RETURNING id, name, description, location, price, available_from, available_to, owner;")
 
     Space.new(id: space[0]['id'], name: space[0]['name'], description: space[0]['description'], location: space[0]['location'], price: space[0]['price'], available_from: space[0]['available_from'], available_to: space[0]['available_to'], owner: space[0]['owner'])
   end
+
+
+  def self.all 
+    spaces = DatabaseConnection.query("SELECT * FROM spaces")
+    
+    spaces.map do |space| 
+      Space.new(id: space['id'], name: space['name'], description: space['description'], location: space['location'], price: space['price'], available_from: space['available_from'], available_to: space['available_to'], owner: space['owner'])
+    end
+    
+  end
+
 end
--- spec/spaces_spec.rb
+++ spec/spaces_spec.rb
@@ -4,7 +4,7 @@ describe 'spaces' do
   it 'can create a space' do
     owner = User.create(name: 'Example', email: 'example@me.com', password: 'password123')
 
-    space = Space.new(name: 'Test Property', description: 'Something here', location: 'London', price: 5.00, available_from: '2020-10-01', available_to: '2020-11-01', owner: owner.id )
+    space = Space.create(name: 'Test Property', description: 'Something here', location: 'London', price: 5.00, available_from: '2020-10-01', available_to: '2020-11-01', owner: owner.id )
 
     expect(space.name).to eq 'Test Property'
     expect(space.location).to eq 'London'
@@ -12,7 +12,16 @@ describe 'spaces' do
     expect(space.available_to).to eq '2020-11-01'
   end
 
-  it 'can show a space' do
+  it 'can show all spaces' do
+    owner = User.create(name: 'Example', email: 'example@me.com', password: 'password123')
+
+    space = Space.create(name: 'Test Property', description: 'Something here', location: 'London', price: 5.00, available_from: '2020-10-01', available_to: '2020-11-01', owner: owner.id )
+
+    all_spaces = Space.all
+    expect(all_spaces.first.name).to eq 'Test Property'
+    expect(all_spaces.first.location).to eq 'London'
+    expect(all_spaces.first.available_from).to eq '2020-10-01'
+    expect(all_spaces.first.available_to).to eq '2020-11-01'
     
   end
 end
