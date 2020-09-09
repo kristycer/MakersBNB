f862877c1cd0d6a6d2f053e072c1aeb48331e975
kristyna cervenkova <kristyna275@gmail.com>
2020-09-09 17:32:50 +0200

    created bookings db and test

--- models/booking.rb
+++ models/booking.rb
@@ -1,7 +1,26 @@
 require './models/database_connection'
 
 class Booking
+    attr_reader :id, :property_name, :booking_date, :total_price, :name, :email
 
-    def make_new_booking
-    end 
+    def initialize(id:, property_name:, booking_date:, total_price:, name:, email:)
+        @id = id
+        @property_name = property_name
+        @booking_date = booking_date
+        @total_price = total_price
+        @name = name
+        @email = email
+      end
+    
+      def self.create(property_name:, booking_date:, total_price:, name:, email:)
+        booking = DatabaseConnection.query(
+          "INSERT INTO requests(property_name, booking_date, total_price, name, email) 
+          VALUES('#{property_name}','#{booking_date}','#{total_price}','#{name}','#{email}') 
+          RETURNING id, property_name, booking_date, total_price, name, email;")
+    
+        Booking.new(
+          id: booking[0]['id'], property_name: booking[0]['property_name'], 
+          booking_date: booking[0]['booking_date'], total_price: booking[0]['total_price'], name: booking[0]['name'],
+          email: booking[0]['email'])
+      end
 end 
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -8,11 +8,11 @@ describe 'booking' do
 
   space = Space.create(name: 'Test Property', description: 'Something here', location: 'London', price: 5.00, available_from: '2020-10-01', available_to: '2020-11-01', owner: owner.id)
 
-  booking = Booking.create(property_name: 'Test Property', booking_date: '2020-10-03' , total_price: 5.00, name_of_booking: 'User Example')
+  booking = Booking.create(property_name: 'Test Property', booking_date: '2020-10-03' , total_price: 5.00, name: 'User Example', email: 'exampleowner@me.com')
 
   expect(booking.property_name).to eq 'Test Property' 
   expect(booking.booking_date).to eq '2020-10-03'
-  expect(booking.name_of_booking).to eq 'User Example'
+  expect(booking.name).to eq 'User Example'
   end
 
 end 
